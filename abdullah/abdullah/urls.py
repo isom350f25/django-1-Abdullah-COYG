5af146c46eba432ba6c6106f52a6797f7bac8f2a
Abdullah <mr3bodx123@gmail.com>
2025-10-19 14:06:49 +0300

    Create urls.py

---
+++ abdullah/abdullah/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for abdullah project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from abdullahapp import views
+
+urlpatterns = [
+    path("myname/", views.myname_view, name="myname"),
+    path("myclasses/", views.myclasses_view, name="myclasses"),
+    path("link/", views.link_view, name="link_view"),
+]
